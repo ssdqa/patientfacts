e2657ef05ea6054bfc55b419b379d402536a9ee9
wieandk <wieandk@chop.edu>
2024-10-31 10:50:29 -0400

    add tests w SQLite db

--- tests/testthat/test-pf_process.R
+++ tests/testthat/test-pf_process.R
@@ -40,27 +40,94 @@ test_that('only omop & pcornet are allowed inputs', {
                           omop_or_pcornet = 'test'))
 })
 
+## Test process function
 
-## Generally checking that code runs
-test_that('testing omop version', {
+test_that('pf ss/ms exp nt -- omop', {
 
-  cht <- data.frame('person_id' = c(1000, 1001),
-                    'site' = c('a', 'b'),
-                    'start_date' = c(as.Date('2007-01-01'),as.Date('2008-01-01')),
-                    'end_date' = c(as.Date('2011-01-01'),as.Date('2009-01-01')))
+  rlang::is_installed("DBI")
+  rlang::is_installed("readr")
+  rlang::is_installed('RSQLite')
 
-  sess <- argos$new()
+  conn <- mk_testdb_omop()
 
-  set_argos_default(sess)
+  initialize_dq_session(session_name = 'pf_process_test',
+                        working_directory = getwd(),
+                        db_conn = conn,
+                        is_json = FALSE,
+                        file_subdirectory = 'testdata',
+                        cdm_schema = NA)
 
-  config('results_name_tag', '')
+  cohort <- cdm_tbl('person') %>% distinct(person_id) %>%
+    mutate(start_date = as.Date(-5000),
+           end_date = as.Date(15000),
+           site = ifelse(person_id %in% c(1:6), 'synth1', 'synth2'))
 
-  expect_error(pf_process(cohort = cht,
-                          multi_or_single_site = 'single',
-                          anomaly_or_exploratory = 'exploratory',
-                          omop_or_pcornet = 'omop'))
+  expect_no_error(pf_process(cohort = cohort,
+                             study_name = 'test',
+                             multi_or_single_site = 'single',
+                             anomaly_or_exploratory = 'exploratory',
+                             omop_or_pcornet = 'omop',
+                             domain_tbl = patientfacts::pf_domain_file %>%
+                               filter(domain == 'diagnoses')))
 })
 
+test_that('pf ss anom nt -- omop', {
+
+  rlang::is_installed("DBI")
+  rlang::is_installed("readr")
+  rlang::is_installed('RSQLite')
+
+  conn <- mk_testdb_omop()
+
+  initialize_dq_session(session_name = 'pf_process_test',
+                        working_directory = getwd(),
+                        db_conn = conn,
+                        is_json = FALSE,
+                        file_subdirectory = 'testdata',
+                        cdm_schema = NA)
+
+  cohort <- cdm_tbl('person') %>% distinct(person_id) %>%
+    mutate(start_date = as.Date(-5000),
+           end_date = as.Date(15000),
+           site = ifelse(person_id %in% c(1:6), 'synth1', 'synth2'))
+
+  expect_no_error(pf_process(cohort = cohort,
+                             study_name = 'test',
+                             multi_or_single_site = 'single',
+                             anomaly_or_exploratory = 'anomaly',
+                             omop_or_pcornet = 'omop',
+                             domain_tbl = patientfacts::pf_domain_file %>%
+                               filter(domain == 'diagnoses')))
+})
+
+test_that('pf ms anom nt -- omop', {
+
+  rlang::is_installed("DBI")
+  rlang::is_installed("readr")
+  rlang::is_installed('RSQLite')
+
+  conn <- mk_testdb_omop()
+
+  initialize_dq_session(session_name = 'pf_process_test',
+                        working_directory = getwd(),
+                        db_conn = conn,
+                        is_json = FALSE,
+                        file_subdirectory = 'testdata',
+                        cdm_schema = NA)
+
+  cohort <- cdm_tbl('person') %>% distinct(person_id) %>%
+    mutate(start_date = as.Date(-5000),
+           end_date = as.Date(15000),
+           site = ifelse(person_id %in% c(1:6), 'synth1', 'synth2'))
+
+  expect_no_error(pf_process(cohort = cohort,
+                             study_name = 'test',
+                             multi_or_single_site = 'multi',
+                             anomaly_or_exploratory = 'anomaly',
+                             omop_or_pcornet = 'omop',
+                             domain_tbl = patientfacts::pf_domain_file %>%
+                               filter(domain == 'diagnoses')))
+})
 
 test_that('testing pcornet version', {
 
@@ -80,3 +147,5 @@ test_that('testing pcornet version', {
                           anomaly_or_exploratory = 'exploratory',
                           omop_or_pcornet = 'pcornet'))
 })
+
+
